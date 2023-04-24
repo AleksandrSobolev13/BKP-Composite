d92a8179d0cf30328ff7dd0bbc05348018f8ded3
Aleksandr Sobolev <130181651+AleksandrSobolev13@users.noreply.github.com>
2023-04-24 21:39:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,12 @@ app = Flask(__name__)
 # Создаем пользовательское приложение для прогнозирования "соотношения матрица-наполнитель"
 # app = flask.Flask(__name__, template_folder = 'templates') 
 # app = Flask(__name__, template_folder = 'templates')
-scaler_x = pickle.load(open('model_scaler_x.pkl', 'rb'))
+
+
+x-file = open('https://github.com/AleksandrSobolev13/BKP-Composite/blob/main/model_scaler_x.pkl', 'rb')
+# scx = pickle(x-file)
+scx = pickle.load(x-file)
+
 # loaded_scaler_y = pickle.load(open('model_scaler_y.pkl', 'rb'))
 # loaded_model = pickle.load(open('model_predict2.pkl', 'rb'))
 
@@ -27,8 +32,8 @@ scaler_x = pickle.load(open('model_scaler_x.pkl', 'rb'))
 # @app.route('/index', methods = ['POST', 'GET'])
 
 def main():
-	# if request.method == 'GET':
-		# return render_template('index1.html') 
+	if request.method == 'GET':
+		return render_template('index.html') 
 	# if request.method == 'POST':
 		# exp1 = request.form.get('experience1')
 		# params.append(exp1)
@@ -38,7 +43,7 @@ def main():
 	# params = []
         # alarm_form = []
 	      	
-	return  render_template('index.html', result = exp1)
+	# return  render_template('index.html', result = exp1)
 
 if __name__ == '__main__':
 	app.run()
